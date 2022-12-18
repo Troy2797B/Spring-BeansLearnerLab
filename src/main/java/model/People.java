a6cea048f12cc68869a99949da56a612c7de9c9b
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-12-18 16:43:07 -0500

    added people

--- src/main/java/model/People.java
+++ src/main/java/model/People.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.Objects;
 
 public abstract class People<PersonType extends Person> implements Iterable<PersonType> {
 
@@ -8,6 +9,32 @@ public abstract class People<PersonType extends Person> implements Iterable<Pers
 
     public People(List<PersonType> personList){
         this.personList = personList;
+    }
+
+    public void add (PersonType type){
+        personList.add(type);
+    }
+    public void remove (PersonType type){
+        personList.remove(type);
+    }
+
+    public int size(){
+        return personList.size();
+    }
+    public void clear(){
+        personList.clear();
+    }
+    public void addAll(Iterable<PersonType> personTypes){
+        for (PersonType p: personTypes) {
+            personList.add(p);
+        }
+    }
+    public PersonType findById(Long id){
+        for (PersonType p: personList) {
+            if (Objects.equals(p.getId(), id)){
+                return p;
+            }
+        } return null;
     }
 
 }
